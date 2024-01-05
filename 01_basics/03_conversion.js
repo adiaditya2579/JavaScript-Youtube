7765132349ed7793266ad998e7cd6954968a3b24
adiaditya2579 <144024145+adiaditya2579@users.noreply.github.com>
2024-01-05 11:32:52 +0000

    conversions & Operations

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,39 @@
+// let score = true;
+
+// console.log(typeof(score))
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber))
+
+// "33" => 33
+// "33abc" =>NaN
+// true => 1 , false => 0
+
+
+
+
+
+// let loggedIn = 1
+// let booleanIsloggedIn = Boolean(loggedIn);
+
+// console.log(booleanIsloggedIn)
+
+// 1 => true , 0 => false, 
+// "" => false, "aditya" => true
+
+
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+
+// console.log(typeof(stringNumber))
+
+
+
+//********************************       Operations *************************/
+
+let value = 3
+let negValue = -value
+console.log(negValue)

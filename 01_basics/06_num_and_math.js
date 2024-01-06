5870f2aae64d9ecd0fb6b17843334a368d67a7a3
adiaditya2579 <144024145+adiaditya2579@users.noreply.github.com>
2024-01-06 19:03:07 +0000

    add to main

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,27 @@
+// const score = 100;
+// console.log(score);
+
+// const balance = new Number (100);
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(4))
+
+
+// const hundreds = 100000000;
+// console.log(hundreds.toLocaleString('en-In'))
+
+
+
+
+//----------------------     Math    ------------------------------------cd--------
+
+
+// console.log(Math.abs(-5))
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(4.9))
+// console.log(Math.round(9.7))
+// console.log(Math.round(9.2))
+
+
+console.log(Math.random())

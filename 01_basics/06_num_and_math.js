0d009ab2119093c239de868c073a409894a44ade
adiaditya2579 <144024145+adiaditya2579@users.noreply.github.com>
2024-01-06 19:19:44 +0000

    add to main

--- 01_basics/06_num_and_math.js
+++ 01_basics/06_num_and_math.js
@@ -24,4 +24,10 @@
 // console.log(Math.round(9.2))
 
 
-console.log(Math.random())
+// console.log(Math.random())
+// console.log((Math.random()*10)+1)
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max - min + 1))+min)
+ 

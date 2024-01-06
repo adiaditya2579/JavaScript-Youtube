d42d3eb913689ebe10c60eba3a56815c43e8af9b
adiaditya2579 <144024145+adiaditya2579@users.noreply.github.com>
2024-01-06 19:57:39 +0000

    About Dates

---
+++ 01_basics/07_date.js
@@ -0,0 +1,16 @@
+// Dates. 
+  
+const myDate = new Date();
+
+// console.log(myDate.toString())
+
+// console.log(myDate.toDateString())
+
+// console.log(myDate.toISOString())
+
+// console.log(myDate.toJSON())
+
+// console.log(myDate.toLocaleDateString())
+
+let myCreatedDate = new Date ()
+con

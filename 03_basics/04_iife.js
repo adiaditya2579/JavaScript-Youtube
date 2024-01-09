41e0c35938fb55d47491783787e012d2e218114a
adiaditya2579 <144024145+adiaditya2579@users.noreply.github.com>
2024-01-09 04:40:49 +0000

    add to main

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+function chai(){
+    console.log(`DB CONNECTED`);
+}
+
+
+(function chai(){
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')

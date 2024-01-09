b21951fd3bc3f2f319857c6389bfbd9cf4cf7a5d
adiaditya2579 <144024145+adiaditya2579@users.noreply.github.com>
2024-01-09 07:48:37 +0000

    add to main

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,6 +1,8 @@
 // if
 const isUserloggedIn = true
 const temperature = 41
+// <, >, <=, >=, ==, !=, ===, !==
+
 
 // if ( temperature === 40 ){
 //     console.log("less than 50");
@@ -8,10 +10,10 @@ const temperature = 41
 //     console.log("temperature is greater than 50");
 // }
 
-// console.log("Execute");
-// <, >, <=, >=, ==, !=, ===, !==
 
-// const score = 200
+
+
+const score = 200
 
 // if (score > 100) {
 //     let power = "fly"
@@ -19,3 +21,21 @@ const temperature = 41
 // }
 
 // console.log(`User power: ${power}`); 
+
+const balance = 1000;
+
+// if (balance > 500) console.log("test"),console.log("test2");   this is not a good way to write if statment also this is currect code 
+
+// if (balance < 500){
+//     console.log("balance is greter then 500")
+// }else if ( balance > 750){
+//     console.log("balace is greter then 750")
+// }
+
+const userLoggedIn = true;
+const debitcard = true;
+const loggedInFromGoogle = false;
+
+if (userLoggedIn && debitcard && loggedInFromGoogle ){
+    console.log("Allow to buy course")
+}
